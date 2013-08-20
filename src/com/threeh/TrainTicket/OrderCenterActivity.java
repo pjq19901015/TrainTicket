fc15b238a85886fa564f6bcac1f126fb5bde9c15
panjiaqiang <378290226@qq.com>
2013-08-20 10:37:38 +0800

    add order center page

--- src/com/threeh/TrainTicket/OrderCenterActivity.java
+++ src/com/threeh/TrainTicket/OrderCenterActivity.java
@@ -1,7 +1,5 @@
 package com.threeh.trainticket;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,14 +13,13 @@ import com.threeh.trainticket.interfaces.ActivityInterface;
  * Time: 下午2:07
  * To change this template use File | Settings | File Templates.
  */
-public class LoginActivity extends BaseActivity implements ActivityInterface{
-    private Button mBtnBack,mBtnLogin;
+public class OrderCenterActivity extends BaseActivity implements ActivityInterface{
+    private Button mBtnBack,mBtnPassengerInformation;
     private TextView mTxtTitle;
-    private String mStrUsername,mStrPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.order_center);
         findView();
         initdata();
         addAction();
@@ -31,31 +28,23 @@ public class LoginActivity extends BaseActivity implements ActivityInterface{
     @Override
     public void findView() {
         mBtnBack = (Button) this.findViewById(R.id.titlebar_btn_back);
-        mBtnLogin = (Button) this.findViewById(R.id.login_btn_login);
+        mBtnPassengerInformation = (Button) this.findViewById(R.id.titlebar_btn_right);
         mTxtTitle = (TextView) this.findViewById(R.id.titlebar_txt_title);
     }
 
     @Override
     public void initdata() {
-
     }
 
     @Override
     public void addAction() {
-        mTxtTitle.setText("登录");
+        mTxtTitle.setText("订单中心");
+        mBtnPassengerInformation.setVisibility(View.VISIBLE);
+        mBtnPassengerInformation.setBackgroundResource(R.drawable.passenger_information_btn_selector);
         mBtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
-            }
-        });
-        mBtnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,OrderCenterActivity.class);
-                intent.putExtra("username",mStrUsername);
-                intent.putExtra("username",mStrPassword);
-                startActivity(intent);
             }
         });
     }
